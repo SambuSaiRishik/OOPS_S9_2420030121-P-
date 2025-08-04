8bc36c4f0223697264ae4b02b5528941cdf49aa5
Sambu Sai Rishik <sambusairishik2007@gmail.com>
2025-08-04 14:31:51 +0530

    Add files via upload

---
+++ string/src/string/ReadAndPrint.java
@@ -0,0 +1,9 @@
+package string;
+
+public class ReadAndPrint {
+	    public static void main(String[] args) {
+	        String str = new String("Hello this is Sai Rishik");
+	        System.out.println(str);
+	    }
+	}
+
---
+++ string/src/string/str_value_with_str_method.java
@@ -0,0 +1,9 @@
+package string;
+import java.util.*;
+public class str_value_with_str_method {
+		    public static void main(String[] args) {
+		        Scanner sc = new Scanner(System.in);
+		        String str = sc.nextLine();
+		        System.out.println("enter string" + str);
+		    }
+		}
