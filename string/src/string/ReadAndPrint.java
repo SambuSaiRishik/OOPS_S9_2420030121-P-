94dabf29f1e16812ab182620c6eb53f6609d75d3
Sambu Sai Rishik <sambusairishik2007@gmail.com>
2025-08-06 11:26:22 +0530

    Delete string directory

--- string/src/string/ReadAndPrint.java
+++
@@ -1,9 +0,0 @@
-package string;
-
-public class ReadAndPrint {
-	    public static void main(String[] args) {
-	        String str = new String("Hello this is Sai Rishik");
-	        System.out.println(str);
-	    }
-	}
-
--- string/src/string/str_value_with_str_method.java
+++
@@ -1,9 +0,0 @@
-package string;
-import java.util.*;
-public class str_value_with_str_method {
-		    public static void main(String[] args) {
-		        Scanner sc = new Scanner(System.in);
-		        String str = sc.nextLine();
-		        System.out.println("enter string" + str);
-		    }
-		}
